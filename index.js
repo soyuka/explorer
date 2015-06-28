a79ddf2766fe2c70cd4be3c00588015fa052aadb
soyuka <soyuka@gmail.com>
2015-06-28 03:03:16 +0200

    Compile dependencies

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-try {
-  var config = require('yamljs').load('./config.yml')
+'use strict';
 
-  if(!config.search) {
-    config.search = {} 
-  } 
-} catch(e) {
-  console.log('No config file!')
-  throw e
+try {
+  var config = require('yamljs').load('./config.yml');
+
+  if (!config.search) {
+    config.search = {};
+  }
+} catch (e) {
+  console.log('No config file!');
+  throw e;
 }
 
-require('./server.js')(config)
-.then(function(app) {
-  return app.listen(config.port || 4859, e => console.log(config.port))
-}) 
+require('./server.js')(config).then(function (app) {
+  return app.listen(config.port || 4859, function (e) {
+    return console.log(config.port);
+  });
+});
