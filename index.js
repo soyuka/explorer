4a50acc464e9a60c5b349e8e2d08e8b58b269fbd
Antoine Bluchet <abluchet@ds-restauration.com>
2015-06-29 12:29:07 +0200

    Compile dependencies

--- index.js
+++ index.js
@@ -1,30 +1,28 @@
-var p = require('path')
+'use strict';
 
-import {firstExistingPath} from './lib/utils.js'
+var _libUtilsJs = require('./lib/utils.js');
+
+var p = require('path');
 
 try {
-  var config_path = firstExistingPath([
-    p.join(process.env.HOME, './.config/explorer/config.yml'), 
-    p.join(__dirname, './config.yml')
-  ])
+  var config_path = (0, _libUtilsJs.firstExistingPath)([p.join(process.env.HOME, './.config/explorer/config.yml'), p.join(__dirname, './config.yml')]);
 
-  var config = require('yamljs').load(config_path)
+  var config = require('yamljs').load(config_path);
 
-  if(!config.search) {
-    config.search = {} 
-  } 
+  if (!config.search) {
+    config.search = {};
+  }
 
-  config.database = p.resolve(p.dirname(config_path), config.database)
+  config.database = p.resolve(p.dirname(config_path), config.database);
 
-  if(!config.port)
-    config.port = 4859
-
-} catch(e) {
-  console.log('No config file!')
-  throw e
+  if (!config.port) config.port = 4859;
+} catch (e) {
+  console.log('No config file!');
+  throw e;
 }
 
-require('./server.js')(config)
-.then(function(app) {
-  return app.listen(config.port, e => console.log(config.port))
-}) 
+require('./server.js')(config).then(function (app) {
+  return app.listen(config.port, function (e) {
+    return console.log(config.port);
+  });
+});
