7337ef77348bc5ad61a7cf26e5ab7a2b12dd2f6c
soyuka <soyuka@gmail.com>
2015-06-29 08:29:15 +0200

    Compile dependencies

--- routes/user.js
+++ routes/user.js
@@ -1,55 +1,59 @@
-var debug = require('debug')('explorer:routes:user')
-var util = require('util')
+'use strict';
 
-const cookieOptions = { httpOnly: false }
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
+var debug = require('debug')('explorer:routes:user');
+var util = require('util');
+
+var cookieOptions = { httpOnly: false };
 
 function home(req, res) {
-  return res.renderBody('login.haml')
+  return res.renderBody('login.haml');
 }
 
 function logout(req, res) {
-  res.cookie('user', {}, util._extend({}, cookieOptions, {expires: new Date()}))
-  return res.redirect('/login')
+  res.cookie('user', {}, util._extend({}, cookieOptions, { expires: new Date() }));
+  return res.redirect('/login');
 }
 
 function login(req, res) {
 
-  if(!req.body.username || !req.body.password) {
-    req.flash('error', 'One of the required fields is missing')
-    return res.redirect('/login') 
+  if (!req.body.username || !req.body.password) {
+    req.flash('error', 'One of the required fields is missing');
+    return res.redirect('/login');
   }
 
-  req.users.authenticate(req.body.username, req.body.password)
-  .then(function(ok) {
+  req.users.authenticate(req.body.username, req.body.password).then(function (ok) {
 
-    debug('Auth %s', ok)
-  
-    if(ok) {
-      let u = req.users.get(req.body.username)
+    debug('Auth %s', ok);
 
-      if(!u) {
-        req.flash('error', `User ${req.body.username} does not exist`)
-        return res.redirect('/login') 
+    if (ok) {
+      var u = req.users.get(req.body.username);
+
+      if (!u) {
+        req.flash('error', 'User ' + req.body.username + ' does not exist');
+        return res.redirect('/login');
       }
 
-      debug('%s logged in', u)
+      debug('%s logged in', u);
 
-      res.cookie('user', {username: u.username, home: u.home, rss: u.rss}, cookieOptions)
+      res.cookie('user', { username: u.username, home: u.home, rss: u.rss }, cookieOptions);
 
-      return res.redirect('/')
-    } 
+      return res.redirect('/');
+    }
 
-    req.flash('error', `Wrong password`)
-    return res.redirect('/login')
-  })  
+    req.flash('error', 'Wrong password');
+    return res.redirect('/login');
+  });
 }
 
-var User = function(app) {
-  app.get('/logout', logout)
-  app.get('/login', home)
-  app.post('/login', login)
+var User = function User(app) {
+  app.get('/logout', logout);
+  app.get('/login', home);
+  app.post('/login', login);
 
-  return app
-}
+  return app;
+};
 
-export {User}
+exports.User = User;
