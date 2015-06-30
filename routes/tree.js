6104aeae378a59481e2103cc42c3a3c47314b429
soyuka <soyuka@gmail.com>
2015-06-30 08:56:35 +0200

    Compile dependencies

--- routes/tree.js
+++ routes/tree.js
@@ -1,14 +1,23 @@
-var fs = require('fs')
-var archiver = require('archiver')
-var rimraf = require('rimraf')
-var p = require('path')
-var debug = require('debug')('explorer:routes:tree')
-var moment = require('moment')
+'use strict';
 
-import {higherPath, extend, buildUrl, secureString} from '../lib/utils.js'
-import {sort} from '../lib/sort.js'
-import { tree } from '../lib/tree.js'
-import { searchMethod } from '../lib/search.js'
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
+
+var _libUtilsJs = require('../lib/utils.js');
+
+var _libSortJs = require('../lib/sort.js');
+
+var _libTreeJs = require('../lib/tree.js');
+
+var _libSearchJs = require('../lib/search.js');
+
+var fs = require('fs');
+var archiver = require('archiver');
+var rimraf = require('rimraf');
+var p = require('path');
+var debug = require('debug')('explorer:routes:tree');
+var moment = require('moment');
 
 /**
  * Prepare tree locals et validate queries 
@@ -16,56 +25,48 @@ import { searchMethod } from '../lib/search.js'
  * @return function middleware(req, res, next)
  */
 function prepareTree(config) {
-  return function(req, res, next) {
+  return function (req, res, next) {
     //should be an app.param
-    if(!req.query.page || req.query.page < 0)
-      req.query.page = 1
+    if (!req.query.page || req.query.page < 0) req.query.page = 1;
 
-    req.query.page = parseInt(req.query.page)
+    req.query.page = parseInt(req.query.page);
 
-    if(req.query.sort) {
-      if(!sort.hasOwnProperty(req.query.sort)) {
-        req.query.sort = null 
+    if (req.query.sort) {
+      if (!_libSortJs.sort.hasOwnProperty(req.query.sort)) {
+        req.query.sort = null;
       }
     }
 
-    if(!~['asc', 'desc'].indexOf(req.query.order)) {
-      req.query.order = 'asc' 
+    if (! ~['asc', 'desc'].indexOf(req.query.order)) {
+      req.query.order = 'asc';
     }
 
-    if(!req.query.path)
-      req.query.path = './'
-    
-    if(req.query.search && config.search.method !== 'native') {
-      req.query.search = secureString(req.query.search)
+    if (!req.query.path) req.query.path = './';
+
+    if (req.query.search && config.search.method !== 'native') {
+      req.query.search = (0, _libUtilsJs.secureString)(req.query.search);
     }
 
-    res.locals = extend(res.locals, {
+    res.locals = (0, _libUtilsJs.extend)(res.locals, {
       search: req.query.search,
       sort: req.query.sort || '',
       order: req.query.order || '',
       page: req.query.page,
       root: p.resolve(req.user.home),
-      path: higherPath(req.user.home, req.query.path),
-      parent: higherPath(req.user.home, p.resolve(req.query.path, '..')),
-      buildUrl: buildUrl,
+      path: (0, _libUtilsJs.higherPath)(req.user.home, req.query.path),
+      parent: (0, _libUtilsJs.higherPath)(req.user.home, p.resolve(req.query.path, '..')),
+      buildUrl: _libUtilsJs.buildUrl,
       remove: config.remove && config.remove.method ? true : false
-    })
+    });
 
-    req.options = extend(
-      res.locals,
-      config.tree, 
-      config.pagination,
-      {remove: config.remove}
-    )
+    req.options = (0, _libUtilsJs.extend)(res.locals, config.tree, config.pagination, { remove: config.remove });
 
-    if(res.locals.sort)
-      req.options.sortMethod = sort[res.locals.sort](req.options)
+    if (res.locals.sort) req.options.sortMethod = _libSortJs.sort[res.locals.sort](req.options);
 
-    debug('Options: %o', req.options)
+    debug('Options: %o', req.options);
 
-    return next()
-  }
+    return next();
+  };
 }
 
 /**
@@ -74,74 +75,73 @@ function prepareTree(config) {
  */
 function compress(req, res) {
 
-  let paths = []
+  var paths = [];
 
-  if(typeof req.body.zip == 'string')
-    req.body.zip = [req.body.zip]
+  if (typeof req.body.zip == 'string') req.body.zip = [req.body.zip];
 
   //validating paths
-  for(let i in req.body.zip) {
-    let path = higherPath(req.user.home, req.body.zip[i]) 
+  for (var i in req.body.zip) {
+    var path = (0, _libUtilsJs.higherPath)(req.user.home, req.body.zip[i]);
 
-    if(path != req.user.home) {
+    if (path != req.user.home) {
       try {
-        var stat = fs.statSync(path)
-      } catch(err) {
-        return res.status(500).send(err)
+        var stat = fs.statSync(path);
+      } catch (err) {
+        return res.status(500).send(err);
       }
 
-      if(stat.isDirectory()) {
-        return res.status(501).send('Can not compress a directory')
+      if (stat.isDirectory()) {
+        return res.status(501).send('Can not compress a directory');
       }
 
-      paths.push(path)
+      paths.push(path);
     }
   }
 
-  let archive = archiver('zip') 
-  let name = req.body.name || 'archive'+new Date().getTime()
+  var archive = archiver('zip');
+  var name = req.body.name || 'archive' + new Date().getTime();
 
-  archive.on('error', function(err) {
-    return res.status(500).send({error: err.message})
-  })
+  archive.on('error', function (err) {
+    return res.status(500).send({ error: err.message });
+  });
 
   //on stream closed we can end the request
-  res.on('close', function() {
-    console.log('Archive wrote %d bytes', archive.pointer())
-    return res.status(200).send('OK')
-  })
+  res.on('close', function () {
+    console.log('Archive wrote %d bytes', archive.pointer());
+    return res.status(200).send('OK');
+  });
 
   //set the archive name
-  res.attachment(`${name}.zip`)
+  res.attachment('' + name + '.zip');
 
   //this is the streaming magic
-  archive.pipe(res)
+  archive.pipe(res);
 
-  for(let i in paths) {
-    archive.append(fs.createReadStream(paths[i]), {name: p.basename(paths[i])}) 
+  for (var i in paths) {
+    archive.append(fs.createReadStream(paths[i]), { name: p.basename(paths[i]) });
   }
 
-  archive.finalize()
+  archive.finalize();
 }
 
 /**
  * @route /download
  */
 function download(req, res) {
-  let path = higherPath(req.user.home, req.query.path)
+  var path = (0, _libUtilsJs.higherPath)(req.user.home, req.query.path);
 
-  if(path === req.user.home) {
-    return res.status(401).send('Unauthorized') 
+  if (path === req.user.home) {
+    return res.status(401).send('Unauthorized');
   }
 
-  return res.download(path, p.basename(path), function(err) {
-    if(err) {
-      console.error('Error %o', err)
-      console.error('With headers %o', res.headersSent)
-      return res.status(500).send('Error while downloading') 
-    } 
-  })
-} 
+  return res.download(path, p.basename(path), function (err) {
+    if (err) {
+      console.error('Error %o', err);
+      console.error('With headers %o', res.headersSent);
+      return res.status(500).send('Error while downloading');
+    }
+  });
+}
 
 /**
  * Get the tree
@@ -149,46 +149,44 @@ function download(req, res) {
  */
 function getTree(req, res) {
 
-  debug('Sort by %s %s', req.options.sort, req.options.order)
+  debug('Sort by %s %s', req.options.sort, req.options.order);
 
-  tree(req.options.path, req.options)
-  .then(function(e) {
-    return res.renderBody('tree.haml', e)
-  })
-  .catch(function(error) {
-    console.error(error)
-    return res.status(500).send('Error while parsing tree at path: ' + req.options.path) 
-  })
+  (0, _libTreeJs.tree)(req.options.path, req.options).then(function (e) {
+    return res.renderBody('tree.haml', e);
+  })['catch'](function (error) {
+    console.error(error);
+    return res.status(500).send('Error while parsing tree at path: ' + req.options.path);
+  });
 }
 
 /**
  * Deletes or moves a file
  */
 function deletePath(req, res) {
-  let path = higherPath(req.user.home, req.query.path)
+  var path = (0, _libUtilsJs.higherPath)(req.user.home, req.query.path);
 
-  debug('Deleting %s', path)
+  debug('Deleting %s', path);
 
-  let next = function(err) {
-    if(err) {
-      console.error(err) 
-      req.flash('error', 'Delete failed')
+  var next = function next(err) {
+    if (err) {
+      console.error(err);
+      req.flash('error', 'Delete failed');
     }
 
-    return res.redirect('back')
+    return res.redirect('back');
+  };
+
+  if (path === req.user.home) {
+    return res.status(401).send('Unauthorized');
   }
 
-  if(path === req.user.home) {
-    return res.status(401).send('Unauthorized') 
-  }
-
-  if(req.options.remove.method == 'mv') {
-    let t = p.join(req.options.remove.trash, p.basename(path) + '.' + moment().format('YYYYMMDDHHmmss'))
-    return fs.rename(path, t, next) 
-  } else if(req.options.remove.method == 'rm') {
-    return rimraf(path, next)
+  if (req.options.remove.method == 'mv') {
+    var t = p.join(req.options.remove.trash, p.basename(path) + '.' + moment().format('YYYYMMDDHHmmss'));
+    return fs.rename(path, t, next);
+  } else if (req.options.remove.method == 'rm') {
+    return rimraf(path, next);
   } else {
-    return res.status(403).send('Forbidden')
+    return res.status(403).send('Forbidden');
   }
 }
 
@@ -197,30 +195,28 @@ function deletePath(req, res) {
  * @route /search
  */
 function search(req, res) {
-  let config = req.config
+  var config = req.config;
 
-  debug('Search with %s', config.search.method, req.options.search)
+  debug('Search with %s', config.search.method, req.options.search);
 
-  searchMethod(config.search.method, config)(req.options.search, req.user.home)
-  .then(function(data) {
-    data = data ? data : this.data.out
-    return tree([].concat.apply([], data), req.options)
-  })
-  .then(function(e) {
-    return res.renderBody('tree.haml', extend(e, {search: req.query.search}))
-  })
+  (0, _libSearchJs.searchMethod)(config.search.method, config)(req.options.search, req.user.home).then(function (data) {
+    data = data ? data : this.data.out;
+    return (0, _libTreeJs.tree)([].concat.apply([], data), req.options);
+  }).then(function (e) {
+    return res.renderBody('tree.haml', (0, _libUtilsJs.extend)(e, { search: req.query.search }));
+  });
 }
 
-var Tree = function(app) {
-  let pt = prepareTree(app.get('config'))
+var Tree = function Tree(app) {
+  var pt = prepareTree(app.get('config'));
 
-  app.get('/', pt, getTree)
-  app.get('/search', pt, search)
-  app.get('/download', download)
-  app.post('/compress', compress)
-  app.get('/remove', pt, deletePath)
+  app.get('/', pt, getTree);
+  app.get('/search', pt, search);
+  app.get('/download', download);
+  app.post('/compress', compress);
+  app.get('/remove', pt, deletePath);
 
-  return app
-}
+  return app;
+};
 
-export {Tree}
+exports.Tree = Tree;
--- server.js
+++ server.js
@@ -1,122 +1,130 @@
-var express = require('express')
-var app = express()
-var p = require('path')
-var util = require('util')
-var hamljs = require('hamljs')
-var cookieParser = require('cookie-parser')
-var bodyParser = require('body-parser')
-var session = require('express-session')
-var flash = require('connect-flash')
-var methodOverride = require('method-override')
-var debug = require('debug')('explorer:server')
-var Promise = require('bluebird')
+'use strict';
 
-import {Users} from './lib/users.js'
-import * as routes from './routes'
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj['default'] = obj; return newObj; } }
 
-module.exports = function(config) {
+var _libUsersJs = require('./lib/users.js');
 
-  app.use(bodyParser.urlencoded({extended: false}))
-  app.use(bodyParser.json())
+var _routes = require('./routes');
 
-  app.use(methodOverride(function(req, res) {
+var routes = _interopRequireWildcard(_routes);
+
+var express = require('express');
+var app = express();
+var p = require('path');
+var util = require('util');
+var hamljs = require('hamljs');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var flash = require('connect-flash');
+var methodOverride = require('method-override');
+var debug = require('debug')('explorer:server');
+var Promise = require('bluebird');
+
+module.exports = function (config) {
+
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
+
+  app.use(methodOverride(function (req, res) {
     if (req.body && typeof req.body === 'object' && '_method' in req.body) {
       // look in urlencoded POST bodies and delete it
-      var method = req.body._method
-      delete req.body._method
-      return method
+      var method = req.body._method;
+      delete req.body._method;
+      return method;
     }
-  }))
+  }));
 
-  app.use(cookieParser())
+  app.use(cookieParser());
   //sessions are only used for flash
-  app.use(session({secret: config.session_secret, resave: false, saveUninitialized: false}))
-  app.use(flash())
-  app.use(express.static('bower_components'))
-  app.use(express.static('client'))
-  app.set('config', config)
-  app.set('view engine','haml' )
+  app.use(session({ secret: config.session_secret, resave: false, saveUninitialized: false }));
+  app.use(flash());
+  app.use(express['static']('bower_components'));
+  app.use(express['static']('client'));
+  app.set('config', config);
+  app.set('view engine', 'haml');
 
-  app.engine('.haml', function(str, options, fn) {
-    options.locals = util._extend({}, options)
+  app.engine('.haml', function (str, options, fn) {
+    options.locals = util._extend({}, options);
     //debug('template locals', options.locals)
-    return hamljs.renderFile(str, 'utf-8', options, fn)
-  })
+    return hamljs.renderFile(str, 'utf-8', options, fn);
+  });
 
-  app.use(function(req, res, next) {
-    res.renderBody = function(name, locals) {
-      locals = util._extend(res.locals, locals ? locals : {})
+  app.use(function (req, res, next) {
+    res.renderBody = function (name, locals) {
+      locals = util._extend(res.locals, locals ? locals : {});
 
-      app.render(name, locals, function(err, body) {
-        return res.render('index.haml', util._extend(locals, {body: body}))
-      }) 
-    }
+      app.render(name, locals, function (err, body) {
+        return res.render('index.haml', util._extend(locals, { body: body }));
+      });
+    };
 
-    req.config = config
-    req.users = users
+    req.config = config;
+    req.users = users;
 
-    res.locals.app_root = config.app_root ? config.app_root : '/'
+    res.locals.app_root = config.app_root ? config.app_root : '/';
 
     res.locals.messages = {
       info: req.flash('info'),
       error: req.flash('error')
-    }
+    };
 
-    return next()
-  })
+    return next();
+  });
 
-  app.use(function(req, res, next) {
+  app.use(function (req, res, next) {
 
-    let user = req.cookies.user
+    var user = req.cookies.user;
 
-    if((!user || !user.username) && req.query.key) {
-      user = req.user = users.getByKey(req.query.key) 
-      
-      if(!req.user) {
-        return res.status(401).send('Key is not valid')
+    if ((!user || !user.username) && req.query.key) {
+      user = req.user = users.getByKey(req.query.key);
+
+      if (!req.user) {
+        return res.status(401).send('Key is not valid');
       }
     }
 
-    if(req.url != '/login' && (!user || !user.username)) {
-      return res.redirect('/login') 
-    } 
-    
-    if(user && user.username && !req.user) {
-      req.user = users.get(user.username)
+    if (req.url != '/login' && (!user || !user.username)) {
+      return res.redirect('/login');
+    }
+
+    if (user && user.username && !req.user) {
+      req.user = users.get(user.username);
 
       //has a bad cookie
-      if(!req.user) {
-        res.cookie('user', {}, util._extend({}, {httpOnly: false}, {expires: -1}))
-        return res.redirect('/login')
+      if (!req.user) {
+        res.cookie('user', {}, util._extend({}, { httpOnly: false }, { expires: -1 }));
+        return res.redirect('/login');
       }
     }
 
-    res.locals.user = req.user || {}
+    res.locals.user = req.user || {};
 
-    debug('User %o', user)
+    debug('User %o', user);
 
-    return next()
-  })
+    return next();
+  });
 
   //Load routes
-  routes.Tree(app)
-  routes.User(app)
-  routes.Settings(app)
-  routes.Admin(app)
+  routes.Tree(app);
+  routes.User(app);
+  routes.Settings(app);
+  routes.Admin(app);
 
   //key?
-  app.get('/rss', function(req, res) {
+  app.get('/rss', function (req, res) {
 
-    if(!req.query.key)
-      return res.send(401, 'Authentication failed')
-    res.send('rss')
-  })
+    if (!req.query.key) return res.send(401, 'Authentication failed');
+    res.send('rss');
+  });
 
   //where is this ES6 feature?
-  var users = new Users({database: p.resolve(__dirname, config.database)})
+  var users = new _libUsersJs.Users({ database: p.resolve(__dirname, config.database) });
 
   //load users from file to memory
-  return users.load()
-  .then(err => console.log('Db loaded'))
-  .then(e => Promise.resolve(app))
-}
+  return users.load().then(function (err) {
+    return console.log('Db loaded');
+  }).then(function (e) {
+    return Promise.resolve(app);
+  });
+};
