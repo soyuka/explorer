16011d9ef9c57f3cd4a2e5d73b68d04925722c6f
soyuka <soyuka@gmail.com>
2015-07-02 08:26:51 +0200

    feat(tree): Keep order and sort in a cookie

--- server.js
+++ server.js
@@ -97,6 +97,29 @@ module.exports = function(config) {
     return next()
   })
 
+  app.use(function(req, res, next) {
+      
+    function isString(v) {return typeof v == 'string'}
+
+    if(isString(req.query.sort) && req.query.sort != req.cookies.sort) {
+      res.cookie('sort', req.query.sort, {httpOnly: false}) 
+    }
+
+    if(isString(req.query.sort) && req.query.order != req.cookies.order) {
+      res.cookie('order', req.query.order, {httpOnly: false}) 
+    }
+
+    if(isString(req.cookies.sort) && !req.query.sort) {
+      req.query.sort = req.cookies.sort
+    }
+
+    if(isString(req.cookies.order) && !req.query.order) {
+      req.query.order = req.cookies.order
+    }
+
+    return next()
+  })
+
   //Load routes
   routes.Tree(app)
   routes.User(app)
