1fd6070be7034e40cb0b7a25d8a5a391399c3b07
soyuka <soyuka@gmail.com>
2015-08-05 20:21:07 +0200

    refactor(middlewares.user): Faster

--- middlewares/user.js
+++ middlewares/user.js
@@ -1,4 +1,5 @@
 import HTTPError from '../lib/HTTPError.js'
+import p from 'path'
 
 let debug = require('debug')('explorer:middlewares:user')
 
@@ -9,42 +10,43 @@ let debug = require('debug')('explorer:middlewares:user')
  */
 function user(req, res, next) {
 
-    let user = req.cookies.user
+  let locals =  {}
+  let user = req.cookies.user
 
-    if((!user || !user.username) && req.query.key) {
-      user = req.user = req.users.getByKey(req.query.key) 
-      
-      if(!req.user) {
-        return res.status(401).send('Key is not valid')
-      }
-    }
-
-    if(req.url != '/login' && (!user || !user.username)) {
-      return next(new HTTPError("Not authenticated", 401, '/login'))
-    } 
+  if((!user || !user.username) && req.query.key) {
+    user = req.user = req.users.getByKey(req.query.key) 
     
-    if(user && user.username && !req.user) {
-      req.user = req.users.get(user.username)
-
-      //has a bad cookie
-      if(!req.user) {
-        res.cookie('user', {}, util._extend({}, {httpOnly: false}, {expires: -1}))
-        return next(new HTTPError("Bad cookie", 400, '/login'))
-      }
+    if(!req.user) {
+      return res.status(401).send('Key is not valid')
     }
-
-    res.locals.user = {}
-
-    if(req.user) {
-      for(let i in req.user) {
-        if(i != 'password')
-          res.locals.user[i] = req.user[i]
-      }
-    }
-
-    debug('User %o', user)
-
-    return next()
   }
+
+  if(req.url != '/login' && (!user || !user.username)) {
+    return next(new HTTPError("Not authenticated", 401, '/login'))
+  } 
+  
+  if(user && user.username && !req.user) {
+    req.user = req.users.get(user.username)
+
+    //has a bad cookie
+    if(!req.user) {
+      res.cookie('user', {}, util._extend({}, {httpOnly: false}, {expires: -1}))
+      return next(new HTTPError("Bad cookie", 400, '/login'))
+    }
+
+    //populating locals
+    for(let i in req.user) {
+      if(i != 'password')
+        locals[i] = req.user[i]
+    }
+
+  }
+
+  debug('User %o', user)
+
+  res.locals.user = locals
+
+  return next()
+} 
 
 export {user}
