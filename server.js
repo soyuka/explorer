6434abd21de26704cc8d65fbc9db00cbf735c29e
soyuka <soyuka@gmail.com>
2015-08-06 23:50:02 +0200

    fix(): Parallel middlewares, move interactor (not linked to server)

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ import {Users} from './lib/users.js'
 import * as routes from './routes'
 import HTTPError from './lib/HTTPError.js'
 import * as middlewares from './middlewares'
-import interactor from './lib/job/interactor.js'
+import {parallelMiddlewares} from './lib/utils.js'
 
 let fs = Promise.promisifyAll(require('fs'))
 let debug = require('debug')('explorer:server')
@@ -27,28 +27,30 @@ module.exports = function(config) {
 
   app.use(bodyParser.urlencoded({extended: false}))
   app.use(bodyParser.json())
-  app.use(cookieParser())
-  //sessions are only used for flash
-  app.use(session({secret: config.session_secret, resave: false, saveUninitialized: false}))
-  app.use(flash())
-  app.use(express.static('client'))
+
   app.set('config', config)
   app.set('view engine','haml' )
-
-  app.use(methodOverride(function(req, res) {
-    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-      // look in urlencoded POST bodies and delete it
-      let method = req.body._method
-      delete req.body._method
-      return method
-    }
-  }))
 
   app.engine('.haml', function(str, options, fn) {
     options.locals = util._extend({}, options)
     //debug('template locals', options.locals)
     return hamljs.renderFile(str, 'utf-8', options, fn)
   })
+
+  app.use(parallelMiddlewares([
+    methodOverride(function(req, res) {
+        if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+          let method = req.body._method
+          delete req.body._method
+          return method
+        }
+    }),
+    cookieParser(),
+    //sessions are only used for flash
+    session({secret: config.session_secret, resave: false, saveUninitialized: false}),
+    flash(),
+    express.static('client')
+  ]))
 
   app.use(function(req, res, next) {
     req.config = config
@@ -64,13 +66,17 @@ module.exports = function(config) {
     return next()
   })
 
-  app.use(middlewares.format(app))
   app.use(middlewares.user)
-  app.use(middlewares.notify)
-  app.use(middlewares.optionsCookie)
+
+  app.use(parallelMiddlewares([
+    middlewares.format(app),
+    middlewares.notify,
+    middlewares.optionsCookie,
+  ]))
 
   //Load routes
   routes.Tree(app)
+  routes.Upload(app)
   routes.User(app)
   routes.Settings(app)
   routes.Admin(app)
@@ -80,21 +86,12 @@ module.exports = function(config) {
   //where is this ES6 feature?
   let users = new Users({database: p.resolve(__dirname, config.database)})
 
-  let plugin_path = p.join(__dirname, './lib/plugins')
-
-  return fs.readdirAsync(plugin_path)
-  .then(function(files) {
-    files = files.map(f => p.join(plugin_path, f))
-
-    if(interactor.job)
-      return Promise.resolve()
-
-    return interactor.run(files)
-  })
-  .then(function() {
-    //load users from file to memory
-    return users.load()
-  })
-  .then(err => !config.quiet ? console.log('Db loaded') : 1)
+  //load users from file to memory
+  return users.load()
+  .then(e => !config.quiet ? console.log('Db loaded') : 1)
   .then(e => Promise.resolve(app))
+  .catch(function(err) {
+    console.error('Error while reading database') 
+    console.error(err.stack)
+  })
 }
