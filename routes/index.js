acb160265ef0b248b794bed170d78bb801bdd3a4
soyuka <soyuka@gmail.com>
2015-08-06 23:48:02 +0200

    feat(): Upload

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,6 @@ import {Tree} from './tree.js'
 import {User} from './user.js'
 import {Admin} from './admin.js'
 import {Settings} from './settings.js'
+import {Upload} from './upload.js'
 
-export {Tree, User, Admin, Settings}
+export {Tree, User, Admin, Settings, Upload}
