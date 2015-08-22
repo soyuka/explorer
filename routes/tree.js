c31d33205c75f79b3e6b895a4726be0afba52a3a
soyuka <soyuka@gmail.com>
2015-08-22 20:32:17 +0200

    Compile dependencies

--- routes/tree.js
+++ routes/tree.js
@@ -1,18 +1,49 @@
-import rimraf from 'rimraf'
-import Promise from 'bluebird'
-import p from 'path'
-import moment from 'moment'
+'use strict';
 
-import {higherPath, extend, removeDirectoryContent, handleSystemError, pathInfo} from '../lib/utils.js'
-import HTTPError from '../lib/HTTPError.js'
-import {tree} from '../lib/tree.js'
-import {searchMethod} from '../lib/search.js'
-import {prepareTree, sanitizeCheckboxes} from '../middlewares'
-import interactor from '../lib/job/interactor.js'
-import Archive from '../lib/plugins/archive.js'
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
 
-let debug = require('debug')('explorer:routes:tree')
-let fs = Promise.promisifyAll(require('fs'))
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+var _rimraf = require('rimraf');
+
+var _rimraf2 = _interopRequireDefault(_rimraf);
+
+var _bluebird = require('bluebird');
+
+var _bluebird2 = _interopRequireDefault(_bluebird);
+
+var _path = require('path');
+
+var _path2 = _interopRequireDefault(_path);
+
+var _moment = require('moment');
+
+var _moment2 = _interopRequireDefault(_moment);
+
+var _libUtilsJs = require('../lib/utils.js');
+
+var _libHTTPErrorJs = require('../lib/HTTPError.js');
+
+var _libHTTPErrorJs2 = _interopRequireDefault(_libHTTPErrorJs);
+
+var _libTreeJs = require('../lib/tree.js');
+
+var _libSearchJs = require('../lib/search.js');
+
+var _middlewares = require('../middlewares');
+
+var _libJobInteractorJs = require('../lib/job/interactor.js');
+
+var _libJobInteractorJs2 = _interopRequireDefault(_libJobInteractorJs);
+
+var _libPluginsArchiveJs = require('../lib/plugins/archive.js');
+
+var _libPluginsArchiveJs2 = _interopRequireDefault(_libPluginsArchiveJs);
+
+var debug = require('debug')('explorer:routes:tree');
+var fs = _bluebird2['default'].promisifyAll(require('fs'));
 
 /**
  * @api {get} /download Download path
@@ -21,50 +52,48 @@ let fs = Promise.promisifyAll(require('fs'))
  * @apiParam {string} path
  */
 function download(req, res, next) {
-  let path = higherPath(req.options.root, req.query.path)
+  var path = (0, _libUtilsJs.higherPath)(req.options.root, req.query.path);
 
-  if(path === req.options.root) {
-    return next(new HTTPError('Unauthorized', 401))
+  if (path === req.options.root) {
+    return next(new _libHTTPErrorJs2['default']('Unauthorized', 401));
   }
 
-  return Promise.join(fs.statAsync(path), pathInfo(path), function(stat, info) {
-    if(stat.isDirectory()) {
-      return next(new HTTPError('Downloading a directory is not possible', 400)) 
+  return _bluebird2['default'].join(fs.statAsync(path), (0, _libUtilsJs.pathInfo)(path), function (stat, info) {
+    if (stat.isDirectory()) {
+      return next(new _libHTTPErrorJs2['default']('Downloading a directory is not possible', 400));
     }
-    
-    if(~['image', 'text'].indexOf(info.type)) {
 
-      debug('SendFile %o', info)
+    if (~['image', 'text'].indexOf(info.type)) {
+
+      debug('SendFile %o', info);
 
       var options = {
         root: req.options.root,
         maxAge: '5h',
         dotfiles: 'deny',
         lastModified: stat.mtime
-      }
+      };
 
-      return res.sendFile(p.relative(options.root, path), options, function(err) {
-        if(err) {
-          return handleSystemError(next)(err)
-        } 
-      })
+      return res.sendFile(_path2['default'].relative(options.root, path), options, function (err) {
+        if (err) {
+          return (0, _libUtilsJs.handleSystemError)(next)(err);
+        }
+      });
     }
 
-    debug('Download %o', info)
+    debug('Download %o', info);
 
-    return res.download(path, p.basename(path), function(err) {
-      if(err) {
-        return handleSystemError(next)(err)
-      } 
-    })
-  })
-  .catch(function(err) {
-    if(err) {
-      return handleSystemError(next)(err)
-    } 
-  })
-
-} 
+    return res.download(path, _path2['default'].basename(path), function (err) {
+      if (err) {
+        return (0, _libUtilsJs.handleSystemError)(next)(err);
+      }
+    });
+  })['catch'](function (err) {
+    if (err) {
+      return (0, _libUtilsJs.handleSystemError)(next)(err);
+    }
+  });
+}
 
 /**
  * @api {get} / Get the tree
@@ -76,16 +105,14 @@ function download(req, res, next) {
  */
 function getTree(req, res, next) {
 
-  debug('Sort by %s %s', req.options.sort, req.options.order)
+  debug('Sort by %s %s', req.options.sort, req.options.order);
 
-  tree(req.options.path, req.options)
-  .then(function(e) {
-    return res.renderBody('tree.haml', e)
-  })
-  .catch(function(err) {
-    console.error('Error while parsing tree at path: ' + req.options.path) 
-    return handleSystemError(next)(err)
-  })
+  (0, _libTreeJs.tree)(req.options.path, req.options).then(function (e) {
+    return res.renderBody('tree.haml', e);
+  })['catch'](function (err) {
+    console.error('Error while parsing tree at path: ' + req.options.path);
+    return (0, _libUtilsJs.handleSystemError)(next)(err);
+  });
 }
 
 /**
@@ -96,34 +123,40 @@ function getTree(req, res, next) {
  */
 function deletePath(req, res, next) {
 
-  let opts = req.options
-  let path = opts.path
+  var opts = req.options;
+  var path = opts.path;
 
-  if(path == opts.root || path == req.user.home) {
-    return next(new HTTPError('Forbidden', 403))
+  if (path == opts.root || path == req.user.home) {
+    return next(new _libHTTPErrorJs2['default']('Forbidden', 403));
   }
 
-  if((!!req.user.readonly) === true || opts.remove.disabled || !~['mv', 'rm'].indexOf(opts.remove.method)) {
-    return next(new HTTPError('Unauthorized', 401))
+  if (!!req.user.readonly === true || opts.remove.disabled || ! ~['mv', 'rm'].indexOf(opts.remove.method)) {
+    return next(new _libHTTPErrorJs2['default']('Unauthorized', 401));
   }
 
-  debug('Deleting %s', path)
+  debug('Deleting %s', path);
 
-  let cb = function(err, newPath) {
-    if(err) {
-      return handleSystemError(next)(err)
+  var cb = function cb(err, newPath) {
+    if (err) {
+      return (0, _libUtilsJs.handleSystemError)(next)(err);
     }
 
-    return res.handle('back', newPath ? {path: newPath, moved: true} : {removed: true})
-  }
+    return res.handle('back', newPath ? { path: newPath, moved: true } : { removed: true });
+  };
 
-  if(opts.remove.method == 'rm') {
-    return rimraf(path, cb)
+  if (opts.remove.method == 'rm') {
+    return (0, _rimraf2['default'])(path, cb);
   } else {
-    let t = p.join(opts.remove.path, p.basename(path) + '.' + moment().format('YYYYMMDDHHmmss'))
-    return fs.rename(path, t, function(err) {
-      return cb(err, t)
-    }) 
+    var _ret = (function () {
+      var t = _path2['default'].join(opts.remove.path, _path2['default'].basename(path) + '.' + (0, _moment2['default'])().format('YYYYMMDDHHmmss'));
+      return {
+        v: fs.rename(path, t, function (err) {
+          return cb(err, t);
+        })
+      };
+    })();
+
+    if (typeof _ret === 'object') return _ret.v;
   }
 }
 
@@ -134,19 +167,16 @@ function deletePath(req, res, next) {
  * @apiParam {string} search
  */
 function search(req, res, next) {
-  let config = req.config
+  var config = req.config;
 
-  debug('Search with %s', config.search.method, req.options.search)
+  debug('Search with %s', config.search.method, req.options.search);
 
-  searchMethod(config.search.method, config)(req.options.search, req.options.root)
-  .then(function(data) {
-    data = data ? data : this.data.out
-    return tree([].concat.apply([], data), req.options)
-  })
-  .then(function(e) {
-    return res.renderBody('tree.haml', extend(e, {search: req.query.search}))
-  })
-  .catch(handleSystemError(next))
+  (0, _libSearchJs.searchMethod)(config.search.method, config)(req.options.search, req.options.root).then(function (data) {
+    data = data ? data : this.data.out;
+    return (0, _libTreeJs.tree)([].concat.apply([], data), req.options);
+  }).then(function (e) {
+    return res.renderBody('tree.haml', (0, _libUtilsJs.extend)(e, { search: req.query.search }));
+  })['catch']((0, _libUtilsJs.handleSystemError)(next));
 }
 
 /**
@@ -156,24 +186,22 @@ function search(req, res, next) {
  */
 function emptyTrash(req, res, next) {
 
-  let opts = req.options
+  var opts = req.options;
 
-  if(opts.remove.disabled || opts.remove.method !== 'mv') {
-    return handleSystemError(next)('Forbidden', 403)
+  if (opts.remove.disabled || opts.remove.method !== 'mv') {
+    return (0, _libUtilsJs.handleSystemError)(next)('Forbidden', 403);
   }
 
-  if(opts.remove.path == opts.root) {
-    return handleSystemError(next)("Won't happend", 417)
+  if (opts.remove.path == opts.root) {
+    return (0, _libUtilsJs.handleSystemError)(next)('Won\'t happend', 417);
   }
 
-  debug('Empty trash %s', opts.remove.path)
+  debug('Empty trash %s', opts.remove.path);
 
-  removeDirectoryContent(opts.remove.path)
-  .then(function() {
-    req.flash('info', 'Trash is now empty!')
-    return res.handle('back')
-  })
-  .catch(handleSystemError(next))
+  (0, _libUtilsJs.removeDirectoryContent)(opts.remove.path).then(function () {
+    req.flash('info', 'Trash is now empty!');
+    return res.handle('back');
+  })['catch']((0, _libUtilsJs.handleSystemError)(next));
 }
 
 /**
@@ -185,52 +213,51 @@ function emptyTrash(req, res, next) {
  * @apiParam {string} action Download, archive, remove
  */
 function treeAction(req, res, next) {
-  if(!req.body.action) {
-    return handleSystemError(next)("Action is needed", 400) 
+  if (!req.body.action) {
+    return (0, _libUtilsJs.handleSystemError)(next)('Action is needed', 400);
   }
 
-  let name = req.body.name || 'archive'+new Date().getTime()
-  let temp = p.join(req.options.archive.path || './', `${name}.zip`)
+  var name = req.body.name || 'archive' + new Date().getTime();
+  var temp = _path2['default'].join(req.options.archive.path || './', '' + name + '.zip');
 
-  let data = {
+  var data = {
     name: name,
     paths: req.options.paths,
     temp: temp,
     directories: req.options.directories,
     root: req.options.root
-  }
+  };
 
   switch (req.body.action) {
     case 'download':
-      data.stream = res
-      let archive = new Archive()
-      return archive.create(data, req.user, req.options)
+      data.stream = res;
+      var archive = new _libPluginsArchiveJs2['default']();
+      return archive.create(data, req.user, req.options);
     case 'archive':
-      if(req.options.archive.disabled)
-        return next(new HTTPError('Unauthorized', 401))
+      if (req.options.archive.disabled) return next(new _libHTTPErrorJs2['default']('Unauthorized', 401));
 
-      data.stream = temp
-      interactor.ipc.send('command', 'archive.create', data, req.user, req.options)
-      return res.handle('back', {info: 'Archive created'}, 201)
+      data.stream = temp;
+      _libJobInteractorJs2['default'].ipc.send('command', 'archive.create', data, req.user, req.options);
+      return res.handle('back', { info: 'Archive created' }, 201);
     case 'remove':
       break;
     default:
-     return handleSystemError(next)("Action must be one of download, archive, remove", 400) 
+      return (0, _libUtilsJs.handleSystemError)(next)('Action must be one of download, archive, remove', 400);
   }
 }
 
-let Tree = function(app) {
-  let config = app.get('config')
-  let pt = prepareTree(config)
+var Tree = function Tree(app) {
+  var config = app.get('config');
+  var pt = (0, _middlewares.prepareTree)(config);
 
-  app.get('/', pt, getTree)
-  app.post('/', pt, sanitizeCheckboxes, treeAction)
-  app.get('/search', pt, search)
-  app.get('/download', pt, download)
-  app.post('/trash', pt, emptyTrash)
-  app.get('/remove', pt, deletePath)
+  app.get('/', pt, getTree);
+  app.post('/', pt, _middlewares.sanitizeCheckboxes, treeAction);
+  app.get('/search', pt, search);
+  app.get('/download', pt, download);
+  app.post('/trash', pt, emptyTrash);
+  app.get('/remove', pt, deletePath);
 
-  return app
-}
+  return app;
+};
 
-export {Tree}
+exports.Tree = Tree;
