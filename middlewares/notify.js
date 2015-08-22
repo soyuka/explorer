428409c12d9be2a59472a725f9289338592e44db
soyuka <soyuka@gmail.com>
2015-08-22 17:40:07 +0200

    Compile dependencies

--- middlewares/notify.js
+++ middlewares/notify.js
@@ -1,56 +1,72 @@
-import interactor from '../lib/job/interactor.js'
-import util from 'util'
-import moment from 'moment'
+'use strict';
 
-let debug = require('debug')('explorer:middlewares:notify')
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+var _libJobInteractorJs = require('../lib/job/interactor.js');
+
+var _libJobInteractorJs2 = _interopRequireDefault(_libJobInteractorJs);
+
+var _util = require('util');
+
+var _util2 = _interopRequireDefault(_util);
+
+var _moment = require('moment');
+
+var _moment2 = _interopRequireDefault(_moment);
+
+var debug = require('debug')('explorer:middlewares:notify');
 
 function notify(req, res, next) {
-    
-  if(!interactor.ipc) {
-    debug('No interactor')
-    res.locals.notifications = {num: 0}
-    return next()
+
+  if (!_libJobInteractorJs2['default'].ipc) {
+    debug('No interactor');
+    res.locals.notifications = { num: 0 };
+    return next();
   }
 
-  interactor.ipc.once('info', function(data) {
+  _libJobInteractorJs2['default'].ipc.once('info', function (data) {
 
-    debug('Notifications %o', data)
+    debug('Notifications %o', data);
 
-    let num = 0
-    let user_data = {}
+    var num = 0;
+    var user_data = {};
 
-    if(!req.user) {
-      res.locals.notifications = {num: num}
-      return next()
+    if (!req.user) {
+      res.locals.notifications = { num: num };
+      return next();
     }
 
-    let notifications = {}
-    let username = req.user.username
+    var notifications = {};
+    var username = req.user.username;
 
-    for(var plugin in data) {
-      if(typeof data[plugin] == 'object') {
-        if(username in data[plugin]) {
-          num += Object.keys(data[plugin][username]).length
-          user_data[plugin] = data[plugin][username]
+    for (var plugin in data) {
+      if (typeof data[plugin] == 'object') {
+        if (username in data[plugin]) {
+          num += Object.keys(data[plugin][username]).length;
+          user_data[plugin] = data[plugin][username];
 
-          for(var i in user_data[plugin]) {
-            user_data[plugin][i].fromNow = moment(user_data[plugin][i].time).fromNow()
+          for (var i in user_data[plugin]) {
+            user_data[plugin][i].fromNow = (0, _moment2['default'])(user_data[plugin][i].time).fromNow();
           }
-
         } else {
-          user_data[plugin] = {} 
+          user_data[plugin] = {};
         }
       }
     }
 
-    debug('User notifications %o', user_data)
+    debug('User notifications %o', user_data);
 
-    res.locals.notifications = util._extend({num: num}, user_data)
+    res.locals.notifications = _util2['default']._extend({ num: num }, user_data);
 
-    return next()
-  })
+    return next();
+  });
 
-  interactor.ipc.send('info')
+  _libJobInteractorJs2['default'].ipc.send('info');
 }
 
-export default notify
+exports['default'] = notify;
+module.exports = exports['default'];
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,21 @@
-import {Tree} from './tree.js'
-import {User} from './user.js'
-import {Admin} from './admin.js'
-import {Settings} from './settings.js'
-import {Upload} from './upload.js'
+'use strict';
 
-export {Tree, User, Admin, Settings, Upload}
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
+
+var _treeJs = require('./tree.js');
+
+var _userJs = require('./user.js');
+
+var _adminJs = require('./admin.js');
+
+var _settingsJs = require('./settings.js');
+
+var _uploadJs = require('./upload.js');
+
+exports.Tree = _treeJs.Tree;
+exports.User = _userJs.User;
+exports.Admin = _adminJs.Admin;
+exports.Settings = _settingsJs.Settings;
+exports.Upload = _uploadJs.Upload;
