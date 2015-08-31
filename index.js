4e97c4968f30739c270dae723248104032a358c4
soyuka <soyuka@gmail.com>
2015-08-31 22:07:40 +0200

    Compile dependencies

--- index.js
+++ index.js
@@ -1,52 +1,71 @@
-import p from 'path'
-import http from 'http'
-import https from 'https'
-import fs from 'fs'
-import interactor from './lib/job/interactor.js'
+'use strict';
 
-import {firstExistingPath} from './lib/utils.js'
-import {getConfiguration} from './lib/config.js'
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+var _path = require('path');
+
+var _path2 = _interopRequireDefault(_path);
+
+var _http = require('http');
+
+var _http2 = _interopRequireDefault(_http);
+
+var _https = require('https');
+
+var _https2 = _interopRequireDefault(_https);
+
+var _fs = require('fs');
+
+var _fs2 = _interopRequireDefault(_fs);
+
+var _libJobInteractorJs = require('./lib/job/interactor.js');
+
+var _libJobInteractorJs2 = _interopRequireDefault(_libJobInteractorJs);
+
+var _libUtilsJs = require('./lib/utils.js');
+
+var _libConfigJs = require('./lib/config.js');
 
 try {
-  let config_path = firstExistingPath([
-    p.join(process.env.HOME || '', './.config/explorer/config.yml'), 
-    p.join(__dirname, './config.yml')
-  ])
+  var config_path = (0, _libUtilsJs.firstExistingPath)([_path2['default'].join(process.env.HOME || '', './.config/explorer/config.yml'), _path2['default'].join(__dirname, './config.yml')]);
 
-  var config = getConfiguration(config_path)
-} catch(e) {
-  console.log('No config file!')
-  throw e
+  var config = (0, _libConfigJs.getConfiguration)(config_path);
+} catch (e) {
+  console.log('No config file!');
+  throw e;
 }
 
-let https_options = {
-  key: fs.readFileSync(config.https.key),
-  cert: fs.readFileSync(config.https.cert)
-}
+var https_options = {
+  key: _fs2['default'].readFileSync(config.https.key),
+  cert: _fs2['default'].readFileSync(config.https.cert)
+};
 
-require('./server.js')(config)
-.then(function(app) {
-  http.createServer(app).listen(config.port, e => !config.quiet ? console.log('HTTP listening on %s', config.port) : 1)
+require('./server.js')(config).then(function (app) {
+  _http2['default'].createServer(app).listen(config.port, function (e) {
+    return !config.quiet ? console.log('HTTP listening on %s', config.port) : 1;
+  });
 
-  if(config.https.enabled) {
-    https.createServer(https_options, app).listen(config.https.port, e => !config.quiet ? console.log('HTTPS listening on %s', config.https.port) : 1)
+  if (config.https.enabled) {
+    _https2['default'].createServer(https_options, app).listen(config.https.port, function (e) {
+      return !config.quiet ? console.log('HTTPS listening on %s', config.https.port) : 1;
+    });
   }
-}) 
+});
 
-let plugin_path = p.join(__dirname, './lib/plugins')
+var plugin_path = _path2['default'].join(__dirname, './lib/plugins');
 
-fs.readdirAsync(plugin_path)
-.then(function(files) {
-  files = files.map(f => p.join(plugin_path, f))
+_fs2['default'].readdirAsync(plugin_path).then(function (files) {
+  files = files.map(function (f) {
+    return _path2['default'].join(plugin_path, f);
+  });
 
-  if(interactor.job) {
-    console.error('Interactor already launched')
-    return Promise.resolve()
+  if (_libJobInteractorJs2['default'].job) {
+    console.error('Interactor already launched');
+    return Promise.resolve();
   }
 
-  return interactor.run(files)
-})
-.catch(function(err) {
-  console.error('Error while launching database') 
-  console.error(err.stack)
-})
+  return _libJobInteractorJs2['default'].run(files);
+})['catch'](function (err) {
+  console.error('Error while launching database');
+  console.error(err.stack);
+});
--- routes/upload.js
+++ routes/upload.js
@@ -1,17 +1,45 @@
-import fs from 'fs'
-import p from 'path'
-import moment from 'moment'
-import multer from 'multer'
+'use strict';
 
-import HTTPError from '../lib/HTTPError.js'
-import {prepareTree} from '../middlewares'
-import interactor from '../lib/job/interactor.js'
-import upload from '../lib/plugins/upload.js'
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
 
-let debug = require('debug')('explorer:routes:upload')
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+var _fs = require('fs');
+
+var _fs2 = _interopRequireDefault(_fs);
+
+var _path = require('path');
+
+var _path2 = _interopRequireDefault(_path);
+
+var _moment = require('moment');
+
+var _moment2 = _interopRequireDefault(_moment);
+
+var _multer = require('multer');
+
+var _multer2 = _interopRequireDefault(_multer);
+
+var _libHTTPErrorJs = require('../lib/HTTPError.js');
+
+var _libHTTPErrorJs2 = _interopRequireDefault(_libHTTPErrorJs);
+
+var _middlewares = require('../middlewares');
+
+var _libJobInteractorJs = require('../lib/job/interactor.js');
+
+var _libJobInteractorJs2 = _interopRequireDefault(_libJobInteractorJs);
+
+var _libPluginsUploadJs = require('../lib/plugins/upload.js');
+
+var _libPluginsUploadJs2 = _interopRequireDefault(_libPluginsUploadJs);
+
+var debug = require('debug')('explorer:routes:upload');
 
 function getUpload(req, res, next) {
-  return res.renderBody('upload', req.options)
+  return res.renderBody('upload', req.options);
 }
 
 /**
@@ -21,50 +49,51 @@ function getUpload(req, res, next) {
  * @apiParam {string} links Links to download
  */
 function remoteUpload(req, res, next) {
-  let links = req.body.links.split('\r\n')
+  var links = req.body.links.split('\r\n');
 
-  links = links.filter(function(e) { return e.trim().length > 0 })
+  links = links.filter(function (e) {
+    return e.trim().length > 0;
+  });
 
-  if(links.length > req.options.upload.maxCount) {
-    return next(new HTTPError(`Max number of files exceeded (${req.options.upload.maxCount})`, 400))
+  if (links.length > req.options.upload.maxCount) {
+    return next(new _libHTTPErrorJs2['default']('Max number of files exceeded (' + req.options.upload.maxCount + ')', 400));
   }
 
-  interactor.ipc.send('call', 'upload.create', links, req.user, req.options)
+  _libJobInteractorJs2['default'].ipc.send('call', 'upload.create', links, req.user, req.options);
 
-  return res.handle('back', {info: 'Upload launched'}, 201)
+  return res.handle('back', { info: 'Upload launched' }, 201);
 }
 
 function canUpload(req, res, next) {
-  let opts = req.options.upload
+  var opts = req.options.upload;
 
-  if(opts.disabled)
-    return next(new HTTPError('Unauthorized', 401))
+  if (opts.disabled) return next(new _libHTTPErrorJs2['default']('Unauthorized', 401));
 
-  return next()
+  return next();
 }
 
-let Upload = function(app) {
-  let config = app.get('config')
-  let pt = prepareTree(config)
+var Upload = function Upload(app) {
+  var config = app.get('config');
+  var pt = (0, _middlewares.prepareTree)(config);
 
-  let storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      return cb(null, req.options.upload.path)
+  var storage = _multer2['default'].diskStorage({
+    destination: function destination(req, file, cb) {
+      return cb(null, req.options.upload.path);
     },
-    filename: function (req, file, cb) {
+    filename: function filename(req, file, cb) {
 
-      let original = file.originalname
-      let ext = p.extname(original)
-      let name = p.basename(original, ext) 
+      var original = file.originalname;
+      var ext = _path2['default'].extname(original);
+      var name = _path2['default'].basename(original, ext);
 
       //rename if exists
-      if(fs.existsSync(p.join(req.options.upload.path, original))) {
-        original =  name + '-' + Date.now() + ext
+      if (_fs2['default'].existsSync(_path2['default'].join(req.options.upload.path, original))) {
+        original = name + '-' + Date.now() + ext;
       }
 
-      return cb(null, original)
+      return cb(null, original);
     }
-  })
+  });
 
   /**
    * @api {post} /upload Upload
@@ -72,23 +101,20 @@ let Upload = function(app) {
    * @apiName upload
    * @apiParam {string[]} files
    */
-  let upload = multer({storage: storage})
+  var upload = (0, _multer2['default'])({ storage: storage });
 
-  app.get('/upload', pt, canUpload, getUpload)
-  app.post('/upload', pt, canUpload, 
-    upload.array('files', config.upload.maxCount),
-    function(req, res, next) {
-      let info = ''
+  app.get('/upload', pt, canUpload, getUpload);
+  app.post('/upload', pt, canUpload, upload.array('files', config.upload.maxCount), function (req, res, next) {
+    var info = '';
 
-      if(req.files.length == 1) {
-        info = `${req.files[0].originalname} uploaded to ${req.files[0].path}`
-      } else {
-        info = `${req.files.length} files uploaded to ${req.options.upload.path}`
-      }
-
-      return res.handle('upload', {info: info}, 200)
+    if (req.files.length == 1) {
+      info = '' + req.files[0].originalname + ' uploaded to ' + req.files[0].path;
+    } else {
+      info = '' + req.files.length + ' files uploaded to ' + req.options.upload.path;
     }
-  )
+
+    return res.handle('upload', { info: info }, 200);
+  });
 
   /**
    * @api {post} /remote-upload Remote Upload
@@ -96,9 +122,9 @@ let Upload = function(app) {
    * @apiName remoteUpload
    * @apiParam {string[]} links One link by line
    */
-  app.post('/remote-upload', pt, canUpload, remoteUpload)
+  app.post('/remote-upload', pt, canUpload, remoteUpload);
 
-  return app
-}
+  return app;
+};
 
-export {Upload}
+exports.Upload = Upload;
