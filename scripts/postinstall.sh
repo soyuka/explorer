b7270359ef9eefa8f13fd33b2dd75af71320413b
soyuka <soyuka@gmail.com>
2015-09-19 16:22:03 +0200

    chore(postinstall): Fix binary link, update plugins (fix #20)

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 if [ ! -z $HOME ]; then
   config_path="$HOME/.config/explorer"
 else
@@ -7,12 +9,11 @@ fi
 
 [ ! -d $config_path ] && mkdir -p $config_path
 cp -R doc/examples/ $config_path
-# [ ! -f $config_path/data/users ] && cp ./doc/examples/data/users $config_path/data/users
-# [ ! -f $config_path/config.yml ] && cp ./doc/examples/config.example.yml $config_path/config.yml
-# [ ! -d $config_path/certs ] && cp -r ./doc/examples/certs $config_path/certs
-# [ ! -d $config_path/trash ] && mkdir $config_path/trash
 
 [ -d $NVM_BIN ] && ln -sf $DIR/bin/explorer $NVM_BIN/explorer
 [ ! -d $NVM_BIN ] && echo "Failed to link binary. Add $DIR/bin to you PATH"
+
+echo "Updating plugins"
+$DIR/bin/explorer plugin install
 
 exit 0
