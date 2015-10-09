7e393c6e52d38b58f74a93f6859dad152f302b5a
soyuka <soyuka@gmail.com>
2015-10-10 00:35:08 +0200

    test(cache): promisify

--- test/cache/cache.js
+++ test/cache/cache.js
@@ -20,14 +20,8 @@ module.exports = function(name, handler, args) {
     })
 
     it('should not store object', function() {
-      return mem.put('test', {foo: 'bar'})
-      .then(function() {
-        throw new Error('It should catch an exception') 
-      })
-      .catch(function(e) {
-        expect(e).to.be.an.instanceof(TypeError) 
-        expect(e.message).to.equal('Key/value arguments must be strings')
-      }) 
+      return expect(mem.put('test', {foo: 'bar'}))
+      .to.be.rejectedWith('Key/value arguments must be strings')
     })
     
     it('should store string', function() {
@@ -35,46 +29,31 @@ module.exports = function(name, handler, args) {
     })
 
     it('should retreive string', function() {
-      return mem.get('foo').then(function(d) {
-        expect(d).to.equal('bar')
-      })
+      return expect(mem.get('foo')).to.eventually.equal('bar')
     })
 
     it('should remove non-existant object', function() {
-      return mem.remove('test').then(function(e) {
-        expect(e).to.be.false
-      })
+      return expect(mem.remove('test')).to.eventually.be.false
     })
 
     it('should create a new instance', function() {
       mem2 = handler.apply(handler, ['test'].concat(args)) 
-      mem.get('foo').then(function(v) {
-        expect(v).to.equal('bar')
-      })
+      return expect(mem.get('foo')).to.eventually.equal('bar')
     })
 
     it('should remove object', function() {
-      return mem.remove('foo')
-      .then(function(e) {
-        expect(e).not.to.be.false
-      })
+      return expect(mem.remove('foo')).not.to.eventually.be.false
     })
 
     it('should not retreive object', function() {
-      return mem.get('foo')
-      .then(function(e) {
-        expect(e).to.equal(null)
-      })
+      return expect(mem.get('foo')).to.eventually.equal(null)
     })
 
     it('should store with a ttl', function() {
-      return mem.put('foottl', 'disappear', 100) 
-      .delay(200)
+      return mem.put('foottl', 'disappear', 1) 
+      .delay(1100)
       .then(function() {
-        return mem.get('foottl')
-      })
-      .then(function(e) {
-        expect(e).to.equal(null) 
+        return expect(mem.get('foottl')).to.eventually.equal(null)
       })
     })
   })
