94bfd491a3b3fedea5ed595b52ca5503553c309b
abluchet <abluchet@ds-restauration.com>
2018-02-14 15:01:08 +0100

    wip

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,17 @@
 var gulp = require('gulp')
-var sass = require('gulp-sass')
-var concat = require('gulp-concat')
 var minify = require('gulp-minify-css')
 var rename = require('gulp-rename')
 var uglify = require('gulp-uglify')
 
 gulp.task('styles', function() {
-  return gulp.src('./client/scss/*.scss')
-  .pipe(sass({
-    includePaths: ['bower_components/foundation/scss']
-  }))
-  .pipe(concat('app.css'))
-  .pipe(gulp.dest('./client/css'))
+  return gulp.src('./client/css/style.css')
   .pipe(minify())
-  .pipe(rename('app.min.css'))
+  .pipe(rename('style.min.css'))
   .pipe(gulp.dest('./client/css'))
 })
 
 gulp.task('javascript', function() {
-  return gulp.src('./client/js/main.js')  
+  return gulp.src('./client/js/main.js')
   .pipe(uglify())
   .pipe(rename('main.min.js'))
   .pipe(gulp.dest('./client/js'))
